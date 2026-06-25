2f79fca0a87b2911ee41e3c101759561e9d1c4c1
spy235 <spyangel235@gmail.com>
2026-06-25 19:11:49 +0530

    added [attendendeceVerification]

--- pages/StudentPage.js
+++ pages/StudentPage.js
@@ -1,3 +1,5 @@
+import { expect } from "playwright/test";
+
 export class StudentPage {
   constructor(page) {
     this.page = page;
@@ -16,10 +18,13 @@ export class StudentPage {
       .locator("input[name='confirmPassword']")
       .fill(student.password);
 
-    await this.page.locator('#subject-select-0').selectOption(student.subject);
+    await this.page.locator("#subject-select-0").selectOption(student.subject);
 
     await this.page.getByPlaceholder("Marks").fill(student.marks);
 
     await this.page.locator("#create-student-btn").click();
+    await expect(
+      await this.page.getByText("Student added successfully!"),
+    ).toBeVisible();
   }
 }
--- test-data/studentFactory.js
+++ test-data/studentFactory.js
@@ -4,6 +4,6 @@ export const createStudentData = () => ({
   name: faker.person.fullName(),
   rollNumber: faker.number.int({ min: 1000, max: 9999 }).toString(),
   password: "Yashas@235@",
-  subject: "Geograpghy",
+  subject: "Geography",
   marks: faker.number.int({ min: 35, max: 100 }).toString(),
 });
